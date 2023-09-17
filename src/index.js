4e93a79aa213a3081d5cdd95a9f4b08d25880b98
Kimberly L Palmer <kimberlylpalmer@gmail.com>
2023-09-17 13:52:26 -0700

    Project complete - Task Lister Lite

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
-document.addEventListener("DOMContentLoaded", () => {
+//document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+//});
+
+//CREATE GLOBAL VARIABLES
+const taskForm = document.querySelector("#create-task-form")
+const taskUl = document.querySelector("#tasks")
+
+function addTask(e) {
+  //block page refreshes
+  e.preventDefault()
+  //determine what the user typed
+  const input = e.target["new-task-description"].value
+  //Add new li to the collectionwith user input as text
+  const newLi = document.createElement('li') //*returns a new Node
+  newLi.textContent = input
+  
+  
+  const btn = document.createElement("button")
+  btn.addEventListener("click", () => newLi.remove())
+  btn.textContent = "X"
+  newLi.append(btn)
+
+  taskUl.appendChild(newLi)
+  //Append the li as child of the Ul
+  e.target.reset()
+}
+
+function attachListeners() {
+  // attach all listeners that we might need
+  taskForm.addEventListener("submit", addTask)
+
+}
+
+
+attachListeners()
