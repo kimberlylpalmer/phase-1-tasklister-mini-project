aed3976b3216e616a460d3a0dc8bee3f8054e9db
Kimberly Palmer <kimberlylpalmer@gmail.com>
2023-11-02 17:00:45 -0700

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,36 +1,29 @@
-//document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
   // your code here
-//});
-
-//CREATE GLOBAL VARIABLES
-const taskForm = document.querySelector("#create-task-form")
-const taskUl = document.querySelector("#tasks")
-
-function addTask(e) {
-  //block page refreshes
-  e.preventDefault()
-  //determine what the user typed
-  const input = e.target["new-task-description"].value
-  //Add new li to the collectionwith user input as text
-  const newLi = document.createElement('li') //*returns a new Node
-  newLi.textContent = input
+  let form = document.getElementById('create-task-form')
   
-  
-  const btn = document.createElement("button")
-  btn.addEventListener("click", () => newLi.remove())
-  btn.textContent = "X"
-  newLi.append(btn)
 
-  taskUl.appendChild(newLi)
-  //Append the li as child of the Ul
-  e.target.reset()
-}
+  form.addEventListener('submit', (e) => {
+      e.preventDefault()
+      handleToDo(e.target["new-task-description"].value)
+      form.reset()
 
-function attachListeners() {
-  // attach all listeners that we might need
-  taskForm.addEventListener("submit", addTask)
+    })
+  })
 
-}
+  function handleToDo(todo) {
+    console.log(todo)
+    let li = document.createElement('li')
+    let btn = document.createElement('button')
+    btn.addEventListener('click', handleDelete) //build this function
+    btn.textContent = "x"
+    li.textContent = `${todo} `
+    li.appendChild(btn)
+    console.log(li)
+    document.getElementById('tasks').appendChild(li);
 
+  }
 
-attachListeners()
+  function handleDelete (e) {
+    e.target.parentNode.remove()
+  }
